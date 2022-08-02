e4e8a39d131fb8ae878e5c98f729d74c90ae0fb4
Argin Aslanian <arginaslaniandeveloper@gmail.com>
2022-08-02 16:55:36 -0700

    Added scanners to read players.txt and teams.txt

--- Main.java
+++ Main.java
@@ -3,10 +3,33 @@
  * Author: Argin Aslanian
  * This program will automate a 30 team NBA Draft with 30 Players and 30 Teams
  * Each team selecting an available player in their turn.
+ * Menu:
+ * 1: NBA Draft -> Conduct the draft
+ * 2: View Available Players -> Display Available Players
+ * 3: View Teams -> Display teams with their draft lottery number
+ * 4: View Results -> Show the results
+ * 5: Exit
  */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("NBA Draft Automated Application");
+        // Initial Message
+        System.out.println("--- WELCOME TO THE NBA DRAFT! ---");
+
+        try {
+            // Scanners to read players.txt and teams.txt
+            Scanner sc_players = new Scanner(new File("players.txt"));
+            Scanner sc_teams = new Scanner(new File("teams.txt"));
+            
+
+        } catch (FileNotFoundException e) {
+            // Message to quit program if File Not Found
+            System.out.println("File was not found: Quitting Application...");
+        }
+        
     }
 }
