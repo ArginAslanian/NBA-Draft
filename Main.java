60b7079e303412e368b277f1c683c0cd80697b65
Argin Aslanian <arginaslaniandeveloper@gmail.com>
2022-08-02 16:23:12 -0700

    initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+/*
+ * NBA Draft Automated Application
+ * Author: Argin Aslanian
+ * This program will automate a 30 team NBA Draft with 30 Players and 30 Teams
+ * Each team selecting an available player in their turn.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("NBA Draft Automated Application");
+    }
+}
