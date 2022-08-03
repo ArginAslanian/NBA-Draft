cab9e0fa45d35821b03275536e15739b82ef6f84
Argin Aslanian <arginaslaniandeveloper@gmail.com>
2022-08-02 17:15:54 -0700

    Loaded ArrayLists from Files

--- Main.java
+++ Main.java
@@ -13,9 +13,22 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    // The ArrayLists will store the teams and players
+    static ArrayList<String> players = new ArrayList<String>();
+    static ArrayList<String> teams = new ArrayList<String>();
+
+    // Function to load teams and players from the files to the ArrayList
+    static void loadFromFile(Scanner file, ArrayList<String> a_list) {
+        while (file.hasNextLine()) {
+            a_list.add(file.nextLine());
+        }
+    }
+
     public static void main(String[] args) {
         // Initial Message
         System.out.println("--- WELCOME TO THE NBA DRAFT! ---");
@@ -25,11 +38,14 @@ public class Main {
             Scanner sc_players = new Scanner(new File("players.txt"));
             Scanner sc_teams = new Scanner(new File("teams.txt"));
             
+            // Files have been loaded.
+            // Now, Load the players and the teams to the ArrayLists
+            loadFromFile(sc_players, players); // Load Players
+            loadFromFile(sc_teams, teams); // Load Teams
 
         } catch (FileNotFoundException e) {
             // Message to quit program if File Not Found
             System.out.println("File was not found: Quitting Application...");
         }
-        
     }
 }
